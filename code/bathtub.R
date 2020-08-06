e9351ca6e248e4bdc65d99930132fa1dca12acfb
Lucas Henneman <lhenneman@gmail.com>
2020-08-06 13:31:39 -0400

    aesthetics improvement, remove tidyverse

--- code/bathtub.R
+++ code/bathtub.R
@@ -1,5 +1,3 @@
-library( tidyverse)
-
 ## ========================================= ##
 #      Define some initial values
 #      n.steps is number of time steps after t = 0
@@ -21,9 +19,9 @@ Qo <- c( 0, rep( 5, 20))
 
 # plot the flows
 par( mfrow = c( 1, 2))
-plot( time, Qi, ylim = c( 0, 10), type = 'l',
+plot( x = time, y = Qi, ylim = c( 0, 10), type = 'l',
       main = 'Bathtub, base model', ylab = 'Inflow, gal/min', xlab = 'time, min')
-plot( time, Qo, ylim = c( 0, 10), type = 'l',
+plot( x = time, y = Qo, ylim = c( 0, 10), type = 'l',
       main = 'Bathtub, base model', ylab = 'Outflow, gal/min', xlab = 'time, min')
 par( mfrow = c( 1, 1))
 
@@ -74,6 +72,14 @@ for( t in 1:n.steps){
 ## ========================================= ##
 #      plot volume across time in feedback model #1
 ## ========================================= ##
+# plot the flows
+par( mfrow = c( 1, 2))
+plot( time, Qi_fb1, ylim = c( 0, 10), type = 'l', 
+      main = 'Bathtub, feeback #1', ylab = 'Inflow, gal/min', xlab = 'time, min')
+plot( time, Qo, ylim = c( 0, 10), type = 'l', 
+      main = 'Bathtub, feeback #1', ylab = 'Outflow, gal/min', xlab = 'time, min')
+par( mfrow = c( 1, 1))
+
 # plot volume
 plot( time, V_fb1, 
       # plot aesthetics
@@ -82,14 +88,6 @@ plot( time, V_fb1,
       main = 'Bathtub, feeback #1',
       ylab = 'Volume, gal',
       xlab = 'time, min')
-
-# plot the flows
-par( mfrow = c( 1, 2))
-plot( time, Qi_fb1, ylim = c( 0, 10), type = 'l', 
-      main = 'Bathtub, feeback #1', ylab = 'Inflow, gal/min', xlab = 'time, min')
-plot( time, Qo, ylim = c( 0, 10), type = 'l', 
-      main = 'Bathtub, feeback #1', ylab = 'Outflow, gal/min', xlab = 'time, min')
-par( mfrow = c( 1, 1))
 
 ## ========================================= ##
 #   Feedback #2: balancing + reinforcing
@@ -112,7 +110,7 @@ Qo_fb2 <- Qo
 # calculate volumes 
 for( t in 1:n.steps){
   Qi_fb2[t + 1] <- ifelse( V_fb2[t] >  45, 2, 6)
-  Qo_fb2[t + 1] <- ifelse( V_fb2[t] >= 45, 10, 8)
+  Qo_fb2[t + 1] <- ifelse( V_fb2[t] >= 30, 8, 12)
   V_fb2[t + 1] <- V_fb2[t] + (Qi_fb2[t + 1] - Qo_fb2[t + 1]) * dt
 }
 
@@ -130,9 +128,9 @@ plot( time, V_fb2,
 
 # plot the flows
 par( mfrow = c( 1, 2))
-plot( time, Qi_fb2, ylim = c( 0, 10), type = 'l', 
+plot( time, Qi_fb2, ylim = c( 0, 12), type = 'l', 
       main = 'Bathtub, feeback #2', ylab = 'Inflow, gal/min', xlab = 'time, min')
-plot( time, Qo_fb2, ylim = c( 0, 10), type = 'l', 
+plot( time, Qo_fb2, ylim = c( 0, 12), type = 'l', 
       main = 'Bathtub, feeback #2', ylab = 'Outflow, gal/min', xlab = 'time, min')
 par( mfrow = c( 1, 1))
 
